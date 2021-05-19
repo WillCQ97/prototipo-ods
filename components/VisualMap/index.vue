e64c2b76d02c308616dd9f5ee2fb23f51fd42d02
WillCQ97 <willian.cqueiroz@gmail.com>
2021-05-19 17:23:13 -0300

    exibição do geojson do campus de alegre

--- components/VisualMap/index.vue
+++ components/VisualMap/index.vue
@@ -3,15 +3,19 @@
     <client-only>
       <l-map :zoom="zoom" :center="center">
         <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-        
+
         <!--este marcador pode ser personalizado-->
         <l-marker :lat-lng="[-20.76161, -41.536]"></l-marker>
+
+        <l-geo-json v-if="show" :geojson="geojson" :options="options" />
       </l-map>
     </client-only>
   </div>
 </template>
 
 <script>
+import alegre_geojson from "assets/campus_alegre";
+
 export default {
   name: "VisualMap",
   data() {
@@ -21,7 +25,45 @@ export default {
       center: [-20.76161, -41.536],
       attribution:
         '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      geojson: alegre_geojson,
+      show: true,
+      fillColor: "#e4ce7f",
+      enableTooltip: true,
     };
+  },
+  computed: {
+    options() {
+      return {
+        onEachFeature: this.onEachFeatureFunction,
+      };
+    },
+    styleFunction() {
+      const fillColor = this.fillColor;
+      return () => {
+        return {
+          weight: 2,
+          color: "#ECEFF1",
+          opacity: 1,
+          fillColor: fillColor,
+          fillOpacity: 1,
+        };
+      };
+    },
+    onEachFeatureFunction() {
+      if (!this.enableTooltip) {
+        return () => {};
+      }
+      return (feature, layer) => {
+        layer.bindTooltip(
+          "<div>ID:" +
+            feature.properties.idd +
+            "</div><div>Nome: " +
+            feature.properties.primario +
+            "</div>",
+          { permanent: false, sticky: true }
+        );
+      };
+    },
   },
 };
 </script>
