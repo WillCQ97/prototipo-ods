b9a82cb9ea283fb3d3ae748e9cf5833f718f23e3
WillCQ97 <willian.cqueiroz@gmail.com>
2021-07-31 20:24:53 -0300

    form update

--- components/ProjectForm.vue
+++ components/ProjectForm.vue
@@ -12,55 +12,59 @@
         :rules="rules"
         v-model="acao"
       ></v-text-field>
-
+    </p>
+    <p>
       <strong>Meta ODS relacionada: </strong>
       <v-text-field
         label="Número da meta e submeta de ODS como, por exemplo, 2.3 ou 11.5"
         :rules="rules"
         v-model="meta"
       ></v-text-field>
-
+    </p>
+    <p>
       <strong>Descrição/Objetivo: </strong>
       <v-textarea
         label="Descrição e objetivos de seu projeto"
         :rules="rules"
         v-model="descricao"
       ></v-textarea>
-
+    </p>
+    <p>
       <strong>Departamento: </strong>
       <v-text-field
         label="Departamento da UFES onde o projeto é desenvolvido"
         :rules="rules"
         v-model="departamento"
       ></v-text-field>
-
+    </p>
+    <p>
       <strong>Coordenador:</strong>
       <v-text-field
         label="Nome do coordenador do projeto"
         :rules="rules"
         v-model="coordenador"
       ></v-text-field>
-
+    </p>
+    <p>
       <strong>Vínculo com a UFES:</strong>
       <v-text-field
         label="Vínculo do coordenador com a UFES, por exemplo, professor"
         :rules="rules"
         v-model="vinculo"
       ></v-text-field>
-
+    </p>
+    <p>
       <strong>E-mail: </strong>
       <v-text-field
         label="E-mail do coordenador do projeto"
         :rules="rules"
         v-model="email"
       ></v-text-field>
-
+    </p>
+    <p>
       <v-dialog v-model="dialog" width="200">
-        <template v-slot:activator="{on, attrs}">
-          <v-btn
-            v-bind="attrs"
-            v-on="on"
-          >Enviar projeto</v-btn>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn v-bind="attrs" v-on="on">Enviar projeto</v-btn>
         </template>
 
         <v-card>
