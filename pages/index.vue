e900529c206c9d05bfe3c4d7b386585a96c0a6b8
WillCQ97 <willian.cqueiroz@gmail.com>
2021-08-10 02:06:26 -0300

    adicionado conteúdo na página inicial

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,44 @@
 <template>
   <div>
-    <h1>Página Inicial</h1>
-    <p>Adicionar algum texto aqui.</p>
+    <div>
+      <v-img src="/ods-ufes-logo.png" max-height="200" max-width="800"></v-img>
+    </div>
+
+    <h1>Objetivos de Desenvolvimento Sustentável (ODS)</h1>
+    <p>
+      A Agenda 2030 consiste em um plano de ação global para governos,
+      sociedade, empresas, academias e pessoas, no qual foram definidos 17
+      objetivos e 169 metas para o fomento de um desenvolvimento mais
+      sustentável das atividades humanas nos próximos anos.  Veja mais
+      <a href="http://www.agenda2030.com.br/sobre/" target="_blank">aqui</a>.
+    </p>
+
+    <h1>A Ufes faz parte dessa iniciativa e conta com você.</h1>
+    <p>
+      A Agenda 2030 é uma oportunidade de a Ufes incorporar os princípios dos
+      ODS nas suas diferentes frentes de atuação (ensino, pesquisa, extensão,
+      assistência e gestão), fortalecendo vínculos e criando conexão entre essas
+      áreas. Em 2020, teve início o projeto institucional de integração dos ODS
+      da Agenda 2030 na Ufes, visando dar transparência às ações de
+      sustentabilidade realizadas pela instituição. Veja mais
+      <a href="http://www.agenda2030.com.br/sobre/" target="_blank">aqui</a>.
+    </p>
+
+    <h1>Cadastre sua contribuição</h1>
+    <p>
+      O mapeamento permitirá o planejamento de ações futuras, o monitoramento
+      dos <em>campi</em> e facilitará a divulgação das boas práticas. Portanto
+      se você coordenou alguma iniciativa que tenha contribuído com um dos ODS,
+      <strong>
+        cadastre sua ação no mapa e mostre sua contribuição nessa importante
+        missão.
+      </strong>
+    </p>
   </div>
 </template>
 
 <script>
 </script>
+
+<style>
+</style>
