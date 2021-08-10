331d496904b457bd86b778ef224af87b8eb56ec1
WillCQ97 <willian.cqueiroz@gmail.com>
2021-08-10 02:44:38 -0300

    adicionado conteúdo seção sobre

--- pages/about.vue
+++ pages/about.vue
@@ -1,27 +1,29 @@
 <template>
   <div>
-    <h1>Sobre o projeto</h1>
-    <h2>Objetivos de Desenvolvimento Sustentável (ODS)</h2>
-    <p>
-      A <strong>Agenda 2030</strong> consiste de um plano de ação global para
-      governos, sociedade, empresas, academias e pessoas. Nele são definidos 17
-      objetivos e 169 metas para o fomento de um desenvolvimento mais
-      sustentável das atividades humanas. Veja mais
-      <a href="http://www.agenda2030.com.br/sobre/">aqui</a>.
-    </p>
+    <h1>Sobre o mapa</h1>
 
-    <h2>Este protótipo</h2>
     <p>
-      A UFES possui diversas ações e projetos relacionadas com a Agenda 2030. E
-      <em>este site protótipo</em> é uma demonstração de como é possível
-      realizar um monitoramento dessas ações de forma colaborativa.
+      A UFES possui diversas ações e projetos relacionados aos ODS da Agenda
+      2030. É importante identificá-las e valorizá-las.
     </p>
     <p>
-      A ideia é que qualquer pessoa com acesso à internet possa escolher algum
-      campus da UFES e visualizar às ações sendo realizadas em um mapa. Além
-      disso, caso o usuário seja algum pesquisador que queira o seu projeto
-      incluído no mapa, ele poderá preencher um formulário e enviar as
-      informações para análise.
+      Este <em>protótipo do site</em> é uma demonstração de como é possível
+      realizar o monitoramento dessas práticas de forma colaborativa.
+    </p>
+    <p>
+      A ideia é que a comunidade acadêmica (docentes, discentes e servidores
+      técnico-administrativos) utilize o <em>login</em>
+      único para acesso à página de inserção de dados e participe ativamente da
+      alimentação desse mapa interativo, através do preenchimento de um
+      formulário, que será validado pela Comissão da Agenda 2030 da UFES ou por
+      uma ouvidoria específica.
+    </p>
+    <p>
+      Além disso, qualquer pessoa com acesso à internet poderá escolher um dos
+      <em>campi</em> da UFES e visualizar as iniciativas realizadas.  O mapa
+      representa, portanto, uma alternativa diferenciada e transparente de
+      comunicação, com participação coletiva e, ao mesmo tempo, uma análise
+      espacial da consolidação dos ODS dentro da UFES.
     </p>
   </div>
 </template>
