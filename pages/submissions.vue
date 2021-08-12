b2447e7ae98a6df1f8d09b676f7824cfd9591b11
WillCQ97 <willian.cqueiroz@gmail.com>
2021-08-12 10:59:16 -0300

    Testes com a página de seleção de projetos

---
+++ pages/submissions.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-card class="mx-auto" max-width="600" tile>
+    <v-list>
+      <v-subheader>Projetos Submetidos</v-subheader>
+      <v-list-item-group v-model="selectedItem" color="primary">
+        <v-list-item
+          three-line
+          v-for="(project, index) in projects"
+          v-bind:key="index"
+        >
+          <v-list-item-content>
+            <v-list-item-title
+              ><strong>Ação: </strong>{{ project.acao }}</v-list-item-title
+            >
+            <v-list-item-subtitle>
+              <strong>Descrição: </strong>{{ project.descricao }}
+            </v-list-item-subtitle>
+            <v-list-item-subtitle>
+              <strong>Coordenador: </strong>{{ project.coordenador.nome }}
+            </v-list-item-subtitle>
+          </v-list-item-content>
+
+          <v-list-item-action>
+            <v-btn icon>
+              <v-icon>mdi-check</v-icon>
+            </v-btn>
+          </v-list-item-action>
+
+          <v-list-item-action>
+            <v-btn icon>
+              <v-icon>mdi-close</v-icon>
+            </v-btn>
+          </v-list-item-action>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-card>
+</template>
+
+<script>
+import submissions from "assets/submissions.json";
+
+export default {
+  name: "PageSubmissions",
+  data() {
+    return {
+      projects: submissions,
+      selectedItem: -1,
+    };
+  },
+};
+</script>
---
+++ pages/submissions2.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-card class="mx-auto" max-width="500">
+    <v-toolbar color="teal" light>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Topics</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-list>
+      <v-list-group
+        v-for="(project, index) in projects"
+        :key="index"
+        v-model="project.active"
+        no-action
+      >
+        <template v-slot:activator>
+          <v-list-item-content>
+            <v-list-item-title
+              ><strong>Ação: </strong>{{ project.acao }}</v-list-item-title
+            >
+          </v-list-item-content>
+        </template>
+
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-subtitle>
+              <strong>Descrição: </strong>
+              {{ project.descricao }}
+            </v-list-item-subtitle>
+            <v-list-item-subtitle>{{project.meta_ods}}</v-list-item-subtitle>
+            <v-list-item-subtitle>{{project.local.centro}}</v-list-item-subtitle>
+            <v-list-item-subtitle>{{project.local.departamento}}</v-list-item-subtitle>
+            <v-list-item-subtitle>{{project.coordenador.nome}}</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-group>
+    </v-list>
+  </v-card>
+</template>
+
+<script>
+import submissions from "assets/submissions.json";
+
+export default {
+  name: "PageSubmissions",
+  data() {
+    return {
+      projects: submissions,
+      selectedItem: -1,
+    };
+  },
+};
+</script>
