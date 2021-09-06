83b3937c1ddb67fdb1f4ba8ab797bc2847565a8d
WillCQ97 <willian.cqueiroz@gmail.com>
2021-09-06 15:37:35 -0300

    Implementação inicial da visualização das submissões

--- pages/submissions.vue
+++ pages/submissions.vue
@@ -1,33 +1,40 @@
 <template>
-  <v-card class="mx-auto" max-width="600" tile>
+  <v-card class="mx-auto" max-width="800" tile>
     <v-list>
       <v-subheader>Projetos Submetidos</v-subheader>
       <v-list-item-group v-model="selectedItem" color="primary">
+        <v-list-item v-if="submissions.length === 0">
+          <v-list-item-content>
+            <v-list-item-title>Não há novos envios.</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
         <v-list-item
-          three-line
-          v-for="(project, index) in projects"
+          two-line
+          v-for="(submission, index) in submissions"
           v-bind:key="index"
         >
           <v-list-item-content>
-            <v-list-item-title
-              ><strong>Ação: </strong>{{ project.action }}</v-list-item-title
-            >
+            <v-list-item-title>
+              <strong>Ação: </strong>{{ submission.project.action }}
+            </v-list-item-title>
             <v-list-item-subtitle>
-              <strong>Descrição: </strong>{{ project.description }}
-            </v-list-item-subtitle>
-            <v-list-item-subtitle>
-              <strong>Coordenador: </strong>{{ project.coordinator.name }}
+              <strong>Descrição: </strong>{{ submission.project.description }}
+              <br />
+              <strong>Coordenador: </strong
+              >{{ submission.project.coordinator.name }}<br />
+              <strong>Data de envio: </strong>{{ submission.date }}<br />
             </v-list-item-subtitle>
           </v-list-item-content>
 
           <v-list-item-action>
-            <v-btn icon>
+            <v-btn icon v-on:click="acceptSubmission(index)">
               <v-icon>mdi-check</v-icon>
             </v-btn>
           </v-list-item-action>
 
           <v-list-item-action>
-            <v-btn icon>
+            <v-btn icon v-on:click="rejectSubmission(index)">
               <v-icon>mdi-close</v-icon>
             </v-btn>
           </v-list-item-action>
@@ -42,9 +49,19 @@ export default {
   name: "PageSubmissions",
   data() {
     return {
-      projects: this.$store.state.submissions.list,
-      selectedItem: -1,
+      submissions: this.$store.state.submissions.list,
+      selectedItem: undefined,
     };
+  },
+  methods: {
+    acceptSubmission(index) {
+      this.$store.commit("projects/add", this.submissions[index].project);
+
+      this.$store.commit("submissions/remove", this.submissions[index]);
+    },
+    rejectSubmission(index) {
+      this.$store.commit("submissions/remove", this.submissions[index]);
+    },
   },
 };
 </script>
